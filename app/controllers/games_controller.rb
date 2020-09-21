1f6f9cd9cc198b37da9e1dc6dae4510bb97d8a6b
April Yeats <yeats.april@gmail.com>
2020-09-21 15:38:30 +0200

    Uncompleted challenge: longest word game - have all the word validation working.. now on to scores

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,14 +10,21 @@ class GamesController < ApplicationController
   def score
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     @html_file = JSON.parse(open(url).read)
+
     if params['word']
+
       if @html_file['found'] == true
-        @answer = "Congratulations! #{params[:word]} is a valid english word!"
         split_word = params[:word].upcase.split('')
         split_letters = params[:letters].split
 
-        split_word.all? do |letter|
+        result = split_word.all? do |letter|
           split_letters.include?(letter)
+        end
+
+        if result == false
+          @answer = "Sorry! #{params[:word]} can't be built from these letters"
+        else
+          @answer = "Congratulations! #{params[:word]} is a valid english word!"
         end
       else
         @answer = "Sorry! #{params[:word]} is not a valid english word."
